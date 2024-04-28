5ab06c2991852ecdb8a76ff0261d5adc66147f81
izhar yefet <izhar.yefet@gmail.com>
2024-04-28 15:43:36 +0300

    Comfy-Store

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, natus.
+        Asperiores totam assumenda eius corporis consequuntur aut iusto corrupti
+        voluptate, laudantium voluptatibus accusamus impedit fuga quasi ut
+        eaque! Facere ipsam dolores repudiandae aspernatur, nulla odit quos
+        expedita molestiae commodi quia.
+      </p>
+    </>
+  )
+}
+export default About
